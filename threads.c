3328bf16469829af1d0830aca693e2a46d754782
Jaykant Tiano <jtiano19@go.byuh.edu>
2020-10-30 23:59:23 +0000

    Add random number generator method

--- threads.c
+++ threads.c
@@ -3,19 +3,22 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <semaphore.h>
+#include <time.h>
 
 
-void *do_math(void *arg);
 void *producer_job(void *arg);
 void *consumer_job(void *arg);
+void addtoA();
+void addtoB();
+int randomNumber();
 
-int sum;
-sem_t a_lock;
-
+int sum_A, sum_B;
+sem_t a_lock, b_lock;
 int main(int argc, char **argv){
     int total_threads;
     pthread_t *threads_array;
-    const int BUFFER_SIZE = 3;
+    sum_A = 0;
+    sum_B = 0;
 
     // Check number of inputs
     if(argc > 1){
@@ -35,7 +38,13 @@ int main(int argc, char **argv){
         perror("Error initializing sum_lock_A");
         return 0;
     }
-
+    if(sem_init(&b_lock,0,1) < 0){
+        perror("Error initializing sum_lock_B");
+        return 0;
+    }
+    
+    //Seed for random generator 
+    srand(time(0)); 
 
     // Launch threads
     printf("Launching %d threads... ( %d producer(s) and %d consumer(s) )\n", total_threads, total_threads/2, total_threads/2);
@@ -58,8 +67,9 @@ int main(int argc, char **argv){
         pthread_join(threads_array[i], NULL);
     }
 
+    printf("A = %d\n", sum_A);
+    printf("B = %d\n", sum_B);
     free(threads_array);
-
 }
 
 // Producer Thread Job
@@ -73,4 +83,10 @@ void *producer_job(void *arg){
 void *consumer_job(void *arg){
     int t_id = (int)(long)arg;
     printf("Consumer %d launched . . . \n", t_id);
+
 }
+
+int randomNumber(){
+    return (rand() % (100 + 1 - 2)) + 2;
+}
+
