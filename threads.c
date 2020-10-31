701fa11c16ace585ece2b55d8be3680b3e351d75
Jaykant Tiano <jtiano19@go.byuh.edu>
2020-10-31 00:11:55 +0000

    Add buffer for producer and consumer

--- threads.c
+++ threads.c
@@ -12,15 +12,18 @@ void addtoA();
 void addtoB();
 int randomNumber();
 
-int sum_A, sum_B;
+int sum_A, sum_B, buffer_count;
 sem_t a_lock, b_lock, s_lock, n_lock, e_lock;
+int *buffer;
 
 int main(int argc, char **argv){
     int total_threads;
     pthread_t *threads_array;
     sum_A = 0;
     sum_B = 0;
-
+    buffer_count = 0;
+    const int BUFFER_SIZE = 3;
+    
     // Check number of inputs
     if(argc > 1){
         printf("Error: No input needed. Do not pass in an argument.\n");
@@ -33,7 +36,8 @@ int main(int argc, char **argv){
     // Initialize number of threads and threads array
     total_threads *= 2;
     threads_array = (pthread_t*) calloc(total_threads,sizeof(pthread_t));
-
+    buffer = (int*) calloc(BUFFER_SIZE, sizeof(int));
+    
     // Initialize semaphores for Part B
     if(sem_init(&a_lock,0,1) < 0){
         perror("Error initializing sum_lock_A");
@@ -84,6 +88,7 @@ int main(int argc, char **argv){
     printf("A = %d\n", sum_A);
     printf("B = %d\n", sum_B);
     free(threads_array);
+    free(buffer);
 }
 
 // Producer Thread Job
